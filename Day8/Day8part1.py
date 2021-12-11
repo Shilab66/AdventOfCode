5052649c647ec89262cae2d65a0787a7662065de
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-11 11:41:48 -0500

    Update Day8part1.py

--- Day8/Day8part1.py
+++ Day8/Day8part1.py
@@ -1,1 +1,21 @@
+file = open("Day8.txt", "r")
+list1 = []
+count = 0
 
+for i in file:
+  lineList = i.split(" | ")
+  output = lineList[1]
+  list1.append(output.split(" "))
+
+for i in list1:
+    last = i[-1]
+    i[-1] = last[:-1]
+
+
+for i in list1:
+    for j in i:
+        if (len(j) == 2 or len(j) == 4 or len(j) == 3 or len(j) == 7):
+            #print(j, len(j))
+            count += 1
+
+print(count)

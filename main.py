94d0c2a7b13e13e904d0813295768614b6759c0c
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-23 09:53:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 print("Just a random file, nothing to see here :>");
-print("Check out the reposertiories to see how I did some of the challenges!")
+print("Check out the repositories to see how I did some of the challenges!")
